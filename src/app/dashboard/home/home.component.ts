ebf9d3fa3b5ab32eee8ca578b0dea6a28d04d882
meanDeveloper <meandeveloper13@gmail.com>
2023-03-27 15:14:11 +0530

    Fix : add pay button in admin table to pay on 18 on every month

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -2,8 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subject, takeUntil } from 'rxjs';
+import { FuseConfirmationService } from 'src/app/shared/module/confirmation/confirmation.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { LoaderService } from 'src/app/shared/services/loader.service';
+import { SnackbarService } from 'src/app/shared/services/snackbar.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
 
@@ -16,7 +18,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   _unsubscribeAll: Subject<any> = new Subject();
 
-  displayedColumns: string[] = ['name', 'email', 'phone_number', 'referral_code', 'is_verified', 'transaction_verified', 'account_balance', 'plan_amount', 'status'];
+  displayedColumns: string[] = ['name', 'email', 'phone_number', 'referral_code', 'is_verified', 'transaction_verified', 'account_balance', 'plan_amount', 'status', 'actions'];
   isLoading: boolean = false;
   pagination: any = {
     length: 0,
@@ -34,7 +36,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor(
     private _http: HttpClient,
     private userService: UserService,
-    private loader: LoaderService
+    private loader: LoaderService,
+    private snackbarService: SnackbarService,
+    private _confirmationService: FuseConfirmationService
   ) { }
 
   ngOnInit(): void {
@@ -69,6 +73,51 @@ export class HomeComponent implements OnInit, OnDestroy {
       }
       this.isLoading = false;
       this.loader.close();
+    });
+  }
+
+  withdrawAmount(user: any) {
+    const confirmation = this._confirmationService.open({
+      title: 'Confirm Payment',
+      message:
+        'Are you sure you want release payment?',
+      icon: {
+        show: true,
+        name: 'warning',
+        color: 'warning',
+      },
+      actions: {
+        confirm: {
+          show: true,
+          label: 'Yes, Pay Now',
+          color: 'primary',
+          class: 'text-slate-100'
+        },
+        cancel: {
+          show: true,
+          label: 'Cancel',
+        },
+      },
+      dismissible: true,
+    });
+
+    confirmation.afterClosed().pipe(takeUntil(this._unsubscribeAll)).subscribe(response => {
+      if (response == 'confirmed') {
+        this.loader.open();
+        this.userService.withdrawBalance(user.user_id).pipe(takeUntil(this._unsubscribeAll)).subscribe(response => {
+          if (response && response.success == true) {
+            this.loader.close();
+            this.getAllUsers(this.pagination.page, this.pagination.size, this.sortObj.active, this.sortObj.direction);
+            this.snackbarService.showSuccess("Transaction Verified Successfully.")
+          }
+        }, error => {
+          console.log('error', error);
+          if (error?.error?.error && 'not_sufficient_amount' in error?.error?.error) {
+            this.snackbarService.showError("Not sufficient amount to withdraw.");
+          }
+          this.loader.close();
+        })
+      }
     });
   }
 
